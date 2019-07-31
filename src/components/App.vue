9cd2b13bcad9a87e443eec4fa15dcee4ec4dadf6
conqueror23 <32027885+conqueror23@users.noreply.github.com>
2019-07-31 20:20:55 +1000

    update

---
+++ src/components/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="app">
+    <NavBar/>
+    <ContentWraper/>
+  </div>
+</template>
+
+<script>
+import NavBar from '@/components/Layouts/NavBar'
+import ContentWraper from '@/components/Layouts/ContentWraper'
+export default {
+  name:'app',
+  components: {
+    NavBar,
+    ContentWraper
+  }
+}
+</script>
+
+<style>
+.app{
+}
+</style>
